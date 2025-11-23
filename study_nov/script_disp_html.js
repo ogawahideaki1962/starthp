77ab94ebc52ae728d1a0e06d60d178f0fac59766
小川秀明 <ogawa.hideaki.fx@gmail.com>
2025-11-23 12:06:27 +0900

    ソース表示を追加 sample_01.html

---
+++ study_nov/script_disp_html.js
@@ -0,0 +1,10 @@
+    // 外部の sample.html を読み込む
+    fetch("sample.html")
+      .then(response => response.text())
+      .then(data => {
+        // そのままテキストとして表示（タグは解釈されない）
+        document.getElementById("output").textContent = data;
+      })
+      .catch(error => {
+        console.error("読み込みエラー:", error);
+      });
