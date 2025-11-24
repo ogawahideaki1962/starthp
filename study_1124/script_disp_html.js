73b3227b911650e4968081764fbcf64cc0d7888e
小川秀明 <ogawa.hideaki.fx@gmail.com>
2025-11-24 15:46:53 +0900

    いっぱい変えた

--- study_1124/script_disp_html.js
+++ study_1124/script_disp_html.js
@@ -1,10 +1,17 @@
-    // 外部の sample.html を読み込む
-    fetch("sample.html")
-      .then(response => response.text())
-      .then(data => {
-        // そのままテキストとして表示（タグは解釈されない）
-        document.getElementById("output").textContent = data;
-      })
-      .catch(error => {
-        console.error("読み込みエラー:", error);
-      });
+function writeSource(filename) {
+  console.log(filename);
+  const newMsg = document.createElement("p");
+  newMsg.textContent = filename;
+  document.body.appendChild(newMsg);
+
+  // 外部の sample.html を読み込む
+  fetch(filename)
+    .then((response) => response.text())
+    .then((data) => {
+      // そのままテキストとして表示（タグは解釈されない）
+      document.getElementById("output").textContent = data;
+    })
+    .catch((error) => {
+      console.error("読み込みエラー:", error);
+    });
+}
